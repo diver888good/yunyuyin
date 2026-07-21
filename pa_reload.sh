e1d83a338354823bc87f8fab6ad56a5f44f4f458
diver888good <3657661653@qq.com>
2026-07-21 21:36:46 +0800

    修复部署冲突：注释git pull，移除多余--user参数

--- pa_reload.sh
+++ pa_reload.sh
@@ -3,17 +3,17 @@
 
 cd /home/music6/yunyuyin
 
-# 拉取最新代码
-git pull
+# CI采用文件上传方式同步代码，注释git pull避免冲突
+# git pull
 
-# 更新依赖
+# 激活虚拟环境，更新依赖
 source /home/music6/venv/bin/activate
-pip install -r requirements.txt --user
+pip install -r requirements.txt
 
 # 数据库迁移
 flask db upgrade
 
-# 重启Celery
+# 重启Celery异步任务
 bash celery_start.sh
 
 echo "网站更新重载完成"
